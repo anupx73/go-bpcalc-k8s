23d3301ba33a6f9632bd15641f38a74603b17565
Anupam Saha <anupam.saha@clover.com>
2023-05-10 16:39:05 +0100

    test: routing rules

--- cmd/app/routes.go
+++ cmd/app/routes.go
@@ -7,12 +7,12 @@ import (
 func (app *application) routes() *mux.Router {
 	// Register handler functions.
 	r := mux.NewRouter()
-	// r.HandleFunc("/api/bpcalc/", app.all).Methods("GET")
-	// r.HandleFunc("/api/bpcalc/{id}", app.findByID).Methods("GET")
-	// r.HandleFunc("/api/bpcalc/", app.insert).Methods("POST")
-	// r.HandleFunc("/api/bpcalc/{id}", app.delete).Methods("DELETE")
-	r.HandleFunc("/", app.all).Methods("GET")
-	r.HandleFunc("/", app.insert).Methods("POST")
+	r.HandleFunc("/api/bpcalc/", app.all).Methods("GET")
+	r.HandleFunc("/api/bpcalc/{id}", app.findByID).Methods("GET")
+	r.HandleFunc("/api/bpcalc/", app.insert).Methods("POST")
+	r.HandleFunc("/api/bpcalc/{id}", app.delete).Methods("DELETE")
+	// r.HandleFunc("/", app.all).Methods("GET")
+	// r.HandleFunc("/", app.insert).Methods("POST")
 
 	return r
 }
